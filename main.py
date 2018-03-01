ddf0c21e63e83e7e5a1abcf7d287b033a6f00a9a
erehonvl@home <erehonvl@gmail.com>
2018-03-01 20:30:35 +0200

    -Distance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 from collections import namedtuple
 
 
@@ -11,6 +12,10 @@ def get_argparser():
     parser.add_argument('-f', type=str)
 
     return parser
+
+
+def calculate_distance(a, b):
+    return math.abs(a[0] - b[0]) + math.abs(a[1] - b[1])
 
 
 def main():
