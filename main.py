7f480d51bfd7b31d8dbaf9fbc22a946df1d2ed1a
Olexander Yermakov <mannavard1611@gmail.com>
2018-03-01 20:08:07 +0200

    Add rides parser

---
+++ main.py
@@ -0,0 +1,35 @@
+import argparse
+from collections import namedtuple
+
+
+Params = namedtuple('Params', field_names=['r', 'c', 'f', 'n', 'b', 't'])
+Ride = namedtuple('Ride', field_names=['a', 'b', 'x', 'y', 's', 'f'])
+
+
+def get_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', type=str)
+
+    return parser
+
+
+def main():
+    args = get_argparser().parse_args()
+
+    rides, params = parse_rides(args)
+
+    print(rides)
+    print(params)
+
+
+def parse_rides(args):
+    with open(args.f, 'r') as f:
+        params = Params(*f.readline().strip().split(' '))
+        rides = []
+        for line in f:
+            rides.append(Ride(*line.strip().split()))
+        return params, rides
+
+
+if __name__ == '__main__':
+    main()
